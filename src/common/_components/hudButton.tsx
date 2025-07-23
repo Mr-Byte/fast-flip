c99a3731cbb11e0e56837d8db8ccee22864b287b
Mr. Byte <bytemr@gmail.com>
2025-07-23 16:18:40 -0700

    Change HUD buttons to use localized tooltip functionality in Foundry.

--- src/common/_components/hudButton.tsx
+++ src/common/_components/hudButton.tsx
@@ -1,11 +1,9 @@
 type HudButtonProps = { onClick: () => void; title: string; icon: string };
 
 export default function HudButton({ onClick, title, icon }: HudButtonProps) {
-    const localizedTitle = game.i18n?.localize(title) ?? "";
-
     return (
-        <button className="control-icon" type="button" onClick={onClick} title={localizedTitle}>
-            <img src={icon} title={title} alt={title} width={36} height={36} />
+        <button className="control-icon" type="button" onClick={onClick} data-tooltip={title}>
+            <img src={icon} width={36} height={36} />
         </button>
     );
 }

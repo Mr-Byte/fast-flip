c7dad18c8a27e717c43ce7af70fbb61bb7fdebc1
Mr. Byte <bytemr@gmail.com>
2025-07-25 00:37:42 -0700

    Change it so that flattened objects only pick primitive leaves.

--- src/common/types/helpers.ts
+++ src/common/types/helpers.ts
@@ -1,15 +1,12 @@
 import { MODULE_NAME } from "@/common/types/index";
-import type { ConditionalExcept, Get, Paths, Split } from "type-fest";
+import type { ConditionalPick, Get, Paths, Primitive, Split } from "type-fest";
 import type { FixPathSquareBrackets } from "type-fest/source/get";
 
 export type WithoutModuleName<T extends string> = T extends `${typeof MODULE_NAME}.${infer K}` ? K : never;
 export type WithModuleName<T extends string> = `${typeof MODULE_NAME}.${string & T}`;
 
-type ToPath<S extends string> = Split<FixPathSquareBrackets<S>, ".", { strictLiteralChecks: false }>;
+export type Flattened<T> = ConditionalPick<FlattenedInner<T>, Primitive>;
 
-export type Flattened<T extends Record<string, unknown>> = ConditionalExcept<
-    {
-        [K in Paths<T>]: Get<T, ToPath<`${K & string}`>>;
-    },
-    object
->;
+type FlattenedInner<T> = {
+    [K in Paths<T>]: Get<T, Split<FixPathSquareBrackets<`${K & string}`>, ".", { strictLiteralChecks: true }>>;
+};
